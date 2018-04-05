1c25cb0cf3e9052c7214552a96d7cf8e32e96df4
USUARIO <USUARIO@PC22>
2018-04-04 21:28:47 -0500

    comit inicio de proyecto

---
+++ src/ejercicio/Ejercicio.java
@@ -0,0 +1,18 @@
+/*
+ .
+ */
+package ejercicio;
+
+/*
+ * @author USUARIO
+ */
+public class Ejercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
